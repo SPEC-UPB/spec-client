b43fc8bf43f63687b291a8052d1cce5938d0d91f
IngDeiver <deiver.guerra.2017@upb.edu.co>
2020-09-17 01:42:16 -0500

    Change ports to prod

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 export default {
-    SERVER_URL:"http://localhost:2050",
-    SERVER_PYTHON_URL:"http://localhost:5000"
+    SERVER_URL:"http://http://radiacionsolar.admin.bucaramanga.upb.edu.co/:2050",
+    SERVER_PYTHON_URL:"http://http://radiacionsolar.bucaramanga.upb.edu.co/:5000"
 }
