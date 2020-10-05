fbece6a1305d98f7fb69ed2119e9c3c5d0ce67e2
IngDeiver <deiver.guerra.2017@upb.edu.co>
2020-10-05 11:25:40 -0500

    PROD

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 export default {
-    SERVER_URL:"http://10.154.12.46:2050",
-    SERVER_PYTHON_URL:"http://10.154.12.46:5000"
+    SERVER_URL:"http://radiacionsolar.admin.bucaramanga.upb.edu.co",
+    SERVER_PYTHON_URL:"http://radiacionsolar.bucaramanga.upb.edu.co:5000"
 }
